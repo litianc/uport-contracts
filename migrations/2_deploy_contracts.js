a416d6d997c920cbdc71f3b55e5d3def275188af
litianc <719153161@qq.come>
2018-10-11 09:07:36 +0800

    Fix: “Error: UportRegistry contract constructor expected 1 arguments, received 0”

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,7 @@ const IdentityFactory = artifacts.require('./IdentityFactory.sol')
 const UportRegistry = artifacts.require('./UportRegistry.sol')
 
 module.exports = function (deployer, network) {
+  var previousPublishedVersion = 0;
   deployer.deploy(IdentityFactory)
-  deployer.deploy(UportRegistry)
+  deployer.deploy(UportRegistry, previousPublishedVersion)
 }
